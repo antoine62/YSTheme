d8a48b6180588ee57bea00a0079f1b11ac15ecdc
antoine62 <antoinejk62@gmail.com>
2019-04-21 21:20:01 +0200

    Trying to fix theme detection.

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -32,10 +32,9 @@
         
           
  std::string themename(int themenum){
- 	
-	std::string strnum = std::to_string(themenum);
-	std::string themepath = "/theme/" + strnum;
+	std::string themepath = "/theme/" + std::to_string(themenum);
 	printf("\x1b[7;1H%s", themepath);
+	printf("\x1b[6;1H/theme/" + std::to_string(themenum))
 	
 	if ((access((themepath + "/YSMenu.ini").c_str(), F_OK) == 0)) {
 		
