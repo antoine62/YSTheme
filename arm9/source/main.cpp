940be38ead4c090f2347e476788c2007500087e1
antoine62 <antoinejk62@gmail.com>
2019-04-21 17:40:46 +0200

    Total N00b Commit #6.6

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -75,7 +75,7 @@ int main(int argc, char **argv) {
 		std::string path = "./";
 		std::string themesname = themename(selected);
 		iprintf("\x1b[1;1Htheme %i - %s \n", selected, themesname.c_str());
-		iprintf("\x1b[5;1HA - Install theme | Dpad left / Dpad right - select theme");
+		iprintf("\x1b[10;1HA - Install theme\nDpad left / Dpad right - select theme");
 	while (true){
 		//Scan nds KEY
 		scanKeys();
