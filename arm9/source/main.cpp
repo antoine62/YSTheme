e4c59831f8f8b1b905561037fcc6a6ab3cf8e7ad
antoine62 <antoinejk62@gmail.com>
2019-04-25 18:50:19 +0200

    small ui bug fix

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -63,9 +63,9 @@ void resetscreen(){
 	printf("\x1b[%i;0H                                  ",i);
 	i++;
 	}
-	iprintf("\x1b[10;1HA - Install theme\nDpad left / Dpad right - select theme\nX - add 10 to theme number.\nB - Remove 10 to theme number.\nY - Make selected theme folder.");
+	iprintf("\x1b[10;1HA - Install theme\n Dpad left / Dpad right - select theme\n X - add 10 to theme number.\n B - Remove 10 to theme number.\n Y - Make selected theme folder.");
 	 		iprintf("\x1b[1;1Htheme %i - %s \n", selected, themename(selected).c_str());
-	 		iprintf("\x1b[20;1HYSTheme ver 1.1 - antoine62\ngithub.com/antoine62/YSTheme");
+	 		iprintf("\x1b[20;1HYSTheme ver 1.1 - antoine62\n github.com/antoine62/YSTheme");
 	}
 
 void stop (void) {
